50230e2b8e72676725829d8c597b532b567923c3
Uvesh99 <sayaidisrar@gmail.com>
2026-04-04 22:30:38 +0530

    deploy on vercal related changes

--- medmas/frontend/vite.config.js
+++ medmas/frontend/vite.config.js
@@ -2,24 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(async ({ mode }) => {
-  const enableHttps = process.env.VITE_DEV_HTTPS === 'true';
-  const plugins = [react()];
-
-  if (enableHttps) {
-    const { default: basicSsl } = await import('@vitejs/plugin-basic-ssl');
-    plugins.push(basicSsl());
-  }
-
-  return {
-    plugins,
-    server: {
-      https: enableHttps,
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-  };
+  },
 });
